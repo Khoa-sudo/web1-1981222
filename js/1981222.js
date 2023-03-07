f29b3118631032068148e5832eb06fe808846512
Silver Wolf <84911530+Khoa-sudo@users.noreply.github.com>
2023-03-07 10:07:40 +0700

    Add pages: about, blogs, contacts, gallery

--- js/1981222.js
+++ js/1981222.js
@@ -1,12 +1,22 @@
 const API = "https://web1-api.vercel.app/api";
 
-async function loadData(request,templateId,viewId) {
+async function loadData(request, templateId, viewId) {
   const response = await fetch(`${API}/${request}`);
-  const data = await response.json();
 
+  const data = await response.json();
   var source = document.getElementById(templateId).innerHTML;
   var template = Handlebars.compile(source);
   var context = { data: data };
   var view = document.getElementById(viewId);
   view.innerHTML = template(context);
 }
+// async function loadBlogs(request, currentPage, templateId, viewId) {
+//   const response = await fetch(`${API}/${request}?page=${currentPage}`);
+
+//   const context = await response.json();
+
+//   var source = document.getElementById(templateId).innerHTML;
+//   var template = Handlebars.compile(source);
+//   var view = document.getElementById(viewId);
+//   view.innerHTML = template(context);
+// }
